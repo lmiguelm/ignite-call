029d6a9078cdf839a32dcc0a31139b96773129ff
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-12-30 16:26:08 -0300

    feat: conectando time-intervals com react-hook-form

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,6 +7,9 @@ import {
   TextInput,
 } from '@lmiguelm-ui/react'
 
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import { ArrowRight } from 'phosphor-react'
 
 import { Container, Header } from '../styles'
@@ -18,8 +21,42 @@ import {
   IntervalItem,
   IntervalsContainer,
 } from './styles'
+import { getWeekDays } from '../../../utils/getWeekDays'
+
+const timeIntervalsFormSchema = z.object({})
 
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitted, errors },
+    control,
+    watch,
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  const weekDays = getWeekDays()
+
+  const { fields } = useFieldArray({
+    name: 'intervals',
+    control,
+  })
+
+  const intervals = watch('intervals')
+
+  async function handleSetTimeIntervals() {}
+
   return (
     <Container>
       <Header>
@@ -33,31 +70,46 @@ export default function TimeIntervals() {
         <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <IntervalsContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <CheckBox />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
+          {fields.map((field, index) => (
+            <IntervalItem key={field.id}>
+              <IntervalDay>
+                <Controller
+                  control={control}
+                  name={`intervals.${index}.enabled`}
+                  render={({ field }) => (
+                    <CheckBox
+                      onCheckedChange={(checked) =>
+                        field.onChange(checked === true)
+                      }
+                      checked={field.value}
+                    />
+                  )}
+                />
 
-            <IntervalInputs>
-              <TextInput size="sm" type="time" step={60} />
-              <TextInput size="sm" type="time" step={60} />
-            </IntervalInputs>
-          </IntervalItem>
+                <Text>{weekDays[field.weekDay]}</Text>
+              </IntervalDay>
 
-          <IntervalItem>
-            <IntervalDay>
-              <CheckBox />
-              <Text>Terça-feira</Text>
-            </IntervalDay>
+              <IntervalInputs>
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  disabled={!intervals[index].enabled}
+                  {...register(`intervals.${index}.startTime`)}
+                />
 
-            <IntervalInputs>
-              <TextInput size="sm" type="time" step={60} />
-              <TextInput size="sm" type="time" step={60} />
-            </IntervalInputs>
-          </IntervalItem>
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  disabled={!intervals[index].enabled}
+                  {...register(`intervals.${index}.endTime`)}
+                />
+              </IntervalInputs>
+            </IntervalItem>
+          ))}
         </IntervalsContainer>
 
         <Button type="submit">
